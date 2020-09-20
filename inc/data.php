4a301e8aef579df31b2c0e54ef9e28d1fd79a9fe
Luciano Neves <atnet@live.com>
2020-09-20 15:12:56 -0300

    Melhorias no data.php correção de bug de charset

--- inc/data.php
+++ inc/data.php
@@ -1,21 +1,29 @@
 <?php
 include_once 'config.php';
+if ( isset( $_POST["query"] ) )
+{
+    $search = $_POST["query"];
+}
+else
+{
+	$search = '';
+}
 try {
-	$connect = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-	
+	$connect = new PDO("mysql:host=$host;dbname=$dbname;charset=UTF8;", $username, $password);	
 } catch (PDOException $pe) {
     die("Não foi possível se conectar ao banco de dados $dbname :" . $pe->getMessage());
 }
+if($search != ''){
 $query = "SELECT * FROM ncm_base 
 	WHERE descricao LIKE :descricao
 	OR codigo LIKE :codigo
 	LIMIT 30
 	";
 	$q=$connect->prepare($query);
-	$q->bindValue(':descricao','%'.$_POST["query"].'%');
-	$q->bindValue(':codigo','%'.$_POST["query"].'%');
+	$q->bindValue(':descricao','%'.$search.'%');
+	$q->bindValue(':codigo','%'.$search.'%');
 	$q->execute();
 	$rows=$q->fetchAll(PDO::FETCH_ASSOC);
-	$json = json_encode($rows);
-	echo $json;
+	print json_encode($rows);
+}
 ?>
